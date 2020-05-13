cbe955538e968372688e4eae19c1076ef90b6e1a
MeherDassThota <t_m_das@optum.com>
2020-05-13 15:29:41 +0530

    Add project files.

---
+++ AzureAgent/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureAgent.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
